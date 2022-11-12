ba5918f3eb3d4efd6a37b59b203b2be3bfe25c88
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-11-12 14:25:06 +0530

    Exercise 2.2
    Course information step7

--- courseinfo/src/components/Course.js
+++ courseinfo/src/components/Course.js
@@ -1,12 +1,16 @@
 import Header from './Header';
 import Content from './Content';
+import Total from './Total';
 
 const Course = ({course}) => {
+    const total = course.parts.map(item => item.exercises)
+                                .reduce((total,num) => total+num);
+
     return ( 
         <div>
             <Header header={course.name}/>
             <Content content={course.parts}/>
-            {/* <Total content={course}/> */}
+            <Total total={total}/>
         </div>
      );
 }
