ce50b42b0d9a61c557ed4b2ec6582524750aaa7c
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-11-12 22:14:37 +0530

    Exercise 2.14
    2.14*: Data for countries, step3

--- country-rest/src/App.js
+++ country-rest/src/App.js
@@ -25,7 +25,6 @@ function App() {
       <CountryList countries={countries.filter(country => 
                           country.name.common.toLowerCase()
                           .includes(filter))}/>
-      
     </div>
   );
 }
--- country-rest/src/Components/CountryDetail.js
+++ country-rest/src/Components/CountryDetail.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const CountryDetail = ({country}) => {
+    const [weather, setWeather] = useState({temp: '', wind: ''});
+    const [loading, setLoading] = useState(false);
+
     const languages = [];
-      for (let key in country.languages) {
+    for (let key in country.languages) {
         languages.push(country.languages[key]);
-      }
+    }
+
+    useEffect(() => {
+      const api_key = process.env.REACT_APP_API_KEY
+      setLoading(true);
+
+      axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.latlng[0]}&lon=${country.latlng[1]}&appid=${api_key}&units=metric`)
+      .then(response => {
+        setLoading(false);
+        setWeather({
+          temp: response.data.main.temp,
+          wind: response.data.wind.speed,
+          icon: response.data.weather[0].icon
+        })
+      })
+    }, [country.latlng])
     
     return ( 
         <div>
@@ -14,6 +36,15 @@ const CountryDetail = ({country}) => {
             {languages.map(language => <li key={language}>{language}</li>)}
             </ul>
             <img src={country.flags.png} alt={country.name.common} />
+            <h2>Weather in {country.capital[0]}</h2>
+            {loading && <p>loading...</p>}
+            {!loading && <div>
+                            <p>temperature {weather.temp} Celsius</p>
+                            {(weather.icon !== undefined) && 
+                              <img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt={weather.icon} />}
+                            <p>wind {weather.wind} m/s</p>
+                          </div>
+            }
       </div>
      );
 }
