ab030863c1a2609f7c1ee6a42de75568e5e43998
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-11-12 16:50:13 +0530

    Exercise 2.8
    The Phonebook Step3

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,12 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '555-55555'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const inputHandler = e => {
+  const nameInputHandler = e => {
     setNewName(e.target.value);
+  }
+
+  const numberInputHandler = e => {
+    setNewNumber(e.target.value);
   }
   
   const checkDuplicate = (obj) => {
@@ -24,10 +31,15 @@ const App = () => {
 
   const submitHandler = e => {
     e.preventDefault();
-    const newPerson = { name: newName }
+    const newPerson = { 
+                        name: newName,
+                        number: newNumber
+                      }
     checkDuplicate(newPerson) 
       ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat(newPerson)) 
+      : setPersons(persons.concat(newPerson))
+    setNewName('');
+    setNewNumber(''); 
   }
 
   return (
@@ -35,7 +47,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={submitHandler}>
         <div>
-          name: <input onChange={inputHandler}/>
+          name: <input value={newName} onChange={nameInputHandler}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={numberInputHandler}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -44,7 +59,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map((person,i) => 
-          <p key={i}>{person.name}</p>
+          <p key={i}>{person.name} {person.number}</p>
         )}
       </div>
     </div>
